bc26acc892090e7d1cdfa6208b6c50214191a6a2
rrstem <rstempie@emich.edu>
2021-10-01 12:18:29 -0400

    impl: HTTP server & router, getDeck API call.
    Implements the HTTP server used by the backend, which is crucial for creating the REST API.
    
    Also implemented the getDeck API call, which will eventually be used to get a full deck from the DB.
    
    Also implemented connecting to the MongoDB.

---
+++ backend/flashfolio/card.go
@@ -0,0 +1,6 @@
+package main;
+
+type Card struct {
+	FrontSide string `json:"FrontSide"`
+	BackSide  string `json:"BackSide"`
+}
--- backend/flashfolio/main.go
+++ backend/flashfolio/main.go
@@ -1,16 +1,86 @@
 package main
 
-
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+//	"io/ioutil"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/handlers"
+
+	"context"
+	"time"
+
+//	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
+const mongoURI string = "mongodb://localhost:27017/"
 
+var mongoClient *mongo.Client
 
 func main() {
 
-	fmt.Println("TODO: Implement Backend")
+	fmt.Println("Starting flashfolio back end...")
+
+	connectToMongo()
+
+	handleRequests()
 
 }
+
+
+func connectToMongo() {
+
+	fmt.Println("Connecting to MongoDB...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var err error
+	mongoClient, err = mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err = mongoClient.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	if err := mongoClient.Ping(ctx, readpref.Primary()); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected to MongoDB")
+}
+
+
+func handleRequests() {
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/getDeck", getDeckReq)
+
+	log.Fatal(http.ListenAndServe(":1337",
+		handlers.CORS(
+		handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}),
+		handlers.AllowedOrigins([]string{"*"}))(router)))
+
+}
+
+
+
+func getDeckReq(w http.ResponseWriter, r *http.Request) {
+	tc := Card{"This is the front :)", "This is the back :("}
+
+	json.NewEncoder(w).Encode(tc)
+}
+
+
 
 
