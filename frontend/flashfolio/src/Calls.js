1c4c844b67519dc182d5a171dace41112e71741e
rrstem <rstempie@emich.edu>
2021-10-06 20:02:43 -0400

    impl: wrapper for API calls
    Create a wrapper library through which API calls will be made. This makes the lives of front end devs much easier and leads to cleaner, easier to maintain front end code.

---
+++ frontend/flashfolio/src/Calls.js
@@ -0,0 +1,21 @@
+
+export const API_URL = "http://localhost:1337"
+
+/*
+getDeck(id: int)
+
+wrapper for getDeck/ call
+*/
+export async function getDeck(deckID) {
+	
+	/* Set up and send req to get deck from backend */
+	let reqOpt = {
+		method: "POST",
+		headers: {"Content-Type": "application/json"},
+		body: JSON.stringify({'ID': Number(deckID)}),
+	}
+
+	/* Send the Request */
+	let resp = await fetch(API_URL + "/getDeck", reqOpt);
+	return resp.json();
+}
--- frontend/flashfolio/src/Viewer.js
+++ frontend/flashfolio/src/Viewer.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import {useParams} from "react-router-dom";
 import Flashcard from "./Flashcard";
-
+import {getDeck} from "./Calls.js";
 
 /*
 Viewer
@@ -11,33 +11,19 @@ Displays a single card at a time to the screen.
 */
 export default function Viewer() {
 
-	let flashdeck = "";
+	const flashdeck = useRef();
 
 	const [flashcard, setFlashcard] = useState("");
 
 	let { deckId } = useParams();
 
 	useEffect(() => {
-
-
-		/* Set up and send req to get deck from backend */
-		let reqOpt = {
-			method: 'post',
-			headers: {'Content-Type': 'application/json'},
-			body: JSON.stringify({'ID': Number(deckId)}),
-		}
-
-		/* Send the Request */
-		fetch('http://localhost:1337/getDeck', reqOpt)
-			.then(resp => resp.json())
-			.then(data => {
-					/* Update the deck to reflect what is on the server 
-					   Warning is ignored as this Effect is only triggered once */
-					// eslint-disable-next-line
-					flashdeck = data;
-					setFlashcard(flashdeck.Cards[0]);
-				});
-	}, [])
+		getDeck(Number(deckId))
+			.then(deck => {
+				flashdeck.current = deck;
+				setFlashcard(flashdeck.current.Cards[0]);
+			});
+	}, [deckId]);
 
 	return (
 		<div>
