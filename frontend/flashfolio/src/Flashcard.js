f49451b0d819d86fb2f061e1fa46acd5b4aa0731
rrstem <rstempie@emich.edu>
2021-10-08 21:04:07 -0400

    impl: Card / Deck Editor
    This allows the user to edit an entire deck / card in a deck. This is needed, as users generally want to be able to edit their cards after having created them.

--- frontend/flashfolio/src/Flashcard.js
+++ frontend/flashfolio/src/Flashcard.js
@@ -1,11 +1,45 @@
-import React, {useState} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import './Flashcard.css'
 
-export default function Flashcard({flashcard}) {
+export default function Flashcard({flashcard, editMode=false}) {
+
 	const [flip, setFlip] = useState(false);
+	const [showEditor, setShowEditor] = useState(false);	
+
+	const currentSide = () => flip ? flashcard.BackSide : flashcard.FrontSide;
+
+	const editor = useRef(null);
+
+	const updateCard = () => {
+		if (flip) {
+			flashcard.BackSide = editor.current.value;
+		} else {
+			flashcard.FrontSide = editor.current.value;
+		}
+	}
+
+	const cardContents = () => {
+		if (!editMode || !showEditor) {
+			return currentSide();
+		} else {
+			return (
+				<textarea className="cardEditor" onChange={updateCard}
+					defaultValue={currentSide()} ref={editor}/>
+			)
+		}
+	}
+
+	const cardClick = () => !editMode || !showEditor ? setFlip(!flip) : "";
+
 	return (
-		<div onClick={() => setFlip(!flip)} class="card">
-			{flip ? flashcard.BackSide : flashcard.FrontSide}	
+		<div>
+		{editMode ? 
+			<button onClick={()=>{setShowEditor(!showEditor)}}>
+				{showEditor ? "Done" : "Edit Card"} 
+			</button>:""}
+		<div onClick={cardClick} class="card">
+			{cardContents()}
+		</div>
 		</div>
 	)
 }
