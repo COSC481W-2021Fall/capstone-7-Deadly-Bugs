10e6be91ccd73cf3251c7c06f2eb9f4a8b03b871
rrstem <rstempie@emich.edu>
2021-10-24 13:21:25 -0400

    Revert "impl: User auth for the front end."
    This reverts commit 0ca1695bd72886cb0683966491b1f3ae0fa7b09d.

--- frontend/flashfolio/src/App.js
+++ frontend/flashfolio/src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Viewer from "./Viewer.js";
-import LoginButton from "./LoginButton.js"
-import { useAuth0 } from "@auth0/auth0-react";
+
 
 /*
 App
@@ -37,14 +36,9 @@ Shown when the user loads the root directory
 TODO: When Hompage PBI is created, move this to it's own component.
 */
 function Home() {
-	const { getTokenSilently, loading, user, logout, isAuthenticated } = useAuth0();
-
 	return (
 		<div>
 			Homepage placeholder
-			<br />
-			<LoginButton />
-			{isAuthenticated ? "Authenticated!" : "Not logged in!" }
 		</div>
 	);
 }
--- frontend/flashfolio/src/index.js
+++ frontend/flashfolio/src/index.js
@@ -1,27 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Auth0Provider } from "@auth0/auth0-react";
-import history from "./history";
-import config from "./auth_config.json"
 
-const onRedirectCallback = appState => {
-	history.push(
-		appState && appState.targetUrl
-			? appState.targetUrl
-			: window.location.pathname
-	);
-};
-
-
-ReactDOM.render(
-	<Auth0Provider
-		domain={config.domain}
-		clientId={config.clientId}
-		redirectUri={window.location.origin}
-		onRedirectCallback={onRedirectCallback}>
-		<App />
-	</Auth0Provider>,
-	document.getElementById("root")
-	);
+ReactDOM.render(<App />, document.getElementById("root"));
 
