7f5af0455380dd9100863718d5232e3af904b36e
rrstem <rstempie@emich.edu>
2021-10-26 07:10:33 -0400

    style: Fix indents on Flashdeck.js
    Replaced spaces (bad) with tabs (good) 🙏

--- frontend/flashfolio/src/Flashdeck.js
+++ frontend/flashfolio/src/Flashdeck.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Flashcard from './Flashcard'
 
 export default function Flashdeck({flashcards}) {
-    return (
-        <div>
-            {flashcards.map(flashcard =>{return <Flashcard flashcard={flashcard}/>})}
-        </div>
-    )
+	return (
+		<div>
+			{flashcards.map(flashcard =>{return <Flashcard flashcard={flashcard}/>})}
+		</div>
+	)
 }
