0e2737e84d98c1aa4e055485fb6c1a951ebb9090
rrstem <rstempie@emich.edu>
2021-10-29 11:19:09 -0400

    impl: add grid layout for the editor
    you can enter the grid-layout editor by hitting the change-layout button on the edit page!

--- frontend/flashfolio/src/Viewer.js
+++ frontend/flashfolio/src/Viewer.js
@@ -3,6 +3,8 @@ import {useParams} from "react-router-dom";
 import Flashcard from "./Flashcard";
 import {getDeck} from "./Calls.js";
 
+import "./Viewer.css";
+
 /*
 Viewer
 
@@ -19,6 +21,8 @@ export default function Viewer({viewMode="view"}) {
 	const [flashcard, setFlashcard] = useState("");
 	const [cardIterator, setCardIterator] = useState(0);
 	const [shufOn, setShufOn] = useState(false);
+
+	const [tileCards, setTileCards] = useState(false);
 
 	function shufFunction(){
 		hidden = false;
@@ -42,6 +46,27 @@ export default function Viewer({viewMode="view"}) {
 	}
 
 	let { deckId } = useParams();
+
+	function changeLayout() {
+		setTileCards(!tileCards)
+	}
+
+
+	function tileLayout() {
+		if (tileCards) {
+			return (
+				<div class="flash-grid">
+					{flashdeck.current.Cards.map(fc =>{
+						return <div><Flashcard flashcard={fc} editMode={viewMode=="edit"}/></div>
+					})}
+				</div>
+			)
+		}
+		return (
+			<Flashcard flashcard={flashcard} editMode={viewMode == "edit"} />
+		)
+
+	}
 
 	useEffect(() => {
 		getDeck(Number(deckId))
@@ -71,17 +96,17 @@ export default function Viewer({viewMode="view"}) {
 		<div>
 		Title: {flashdeck.current.Title}
 		DeckId: {deckId}
-		<Flashcard flashcard={flashcard} editMode={viewMode == "edit"} />
-		<button
+		<br/>
+		{viewMode == "edit" && <button onClick={changeLayout}>Change Layout</button>}
+		{tileLayout()}
+		{!tileCards && <button
 			onClick={() => setCardIterator(cardIterator + 1)}
-			>Next Card</button>
-		{!flashcard.showEditor ? 
-		hidden ?
+			>Next Card</button>}
+		{viewMode=="view" && 
+		(hidden ?
 		<button id= "shuf" onClick = {() => shufFunction()}>Shuffle</button> :
-		<button id="unshuf" onClick = {() => shufOn ? unshufFunction() : null}>Unshuffle</button>
-		:
-		null
-	}
+		<button id="unshuf" onClick = {() => shufOn ? unshufFunction() : null}>Unshuffle</button>)
+		}
 		<a
             href={`data:text/json;charset=utf-8,${encodeURIComponent(
               JSON.stringify(flashdeck.current, null, '\t')
